9b5a509905068b46ca01b7699a91397049740425
Iko Lin <iko@dacard.cc>
2022-12-22 16:57:32 +0800

    feat: add useRouter example

--- app/client/page.js
+++ app/client/page.js
@@ -6,14 +6,17 @@ import Image from "next/image";
 
 export default function Page() {
   const router = useRouter();
+  console.log("🚀 ~ file: page.js:9 ~ Page ~ router", router);
 
   return (
     <div>
       <p>client component</p>
-      <Image src={leaf} alt="leaf" width={300} height={800} />
-      <button type="button" onClick={() => router.push("/useRouter")}>
-        go to page by router push
-      </button>
+      <div>
+        <button type="button" onClick={() => router.push("/useRouter")}>
+          go to page by router push
+        </button>
+      </div>
+      <Image src={leaf} alt="leaf" width={300} height={400} />
     </div>
   );
 }
--- app/useRouter/page.js
+++ app/useRouter/page.js
@@ -1,4 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+
 export default function Page() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  console.log("🚀 ~ file: page.js:7 ~ Page ~ pathname", pathname);
+  console.log("🚀 ~ file: page.js:9 ~ Page ~ searchParams", searchParams);
+
   return (
     <div>
       <p>from useRouter</p>
